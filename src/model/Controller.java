2f95a70d548efe7f43e0695a0ead20abc7636efd
JeanDieTwice21 <atkinsonvi2@gmail.com>
2023-03-31 16:06:46 -0500

    Project finished

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -5,37 +5,122 @@ import java.util.GregorianCalendar;
 
 public class Controller {
 
+	public static final int SIZE_ARRAY = 10;
 	private Project[] projects;
+	private String[] namesAfterDate;
+	private String[] namesBeforeDate;
 
 	public Controller() {
 
-		projects = new Project[10];
-	
+		projects = new Project[SIZE_ARRAY];
+		namesAfterDate = new String[10];
+		namesBeforeDate = new String[10];
 	}
 	
-	//Incomplete
-	public boolean RegisterProject() {
+	public boolean registerProject(String name, String clientName, Calendar initialDate, Calendar finalDate, double budget) {
 
-		return false;
+		Project project = new Project(name, clientName, initialDate, finalDate, budget);
+		int pos = getFirstValidPosition();
+		if(pos != -1){
+			projects[pos] = project;
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsAfterDate() {
+ 	public String searchProjectsAfterDate(Calendar dateToCompare) {
 
-		String msg = "";
+		String msg = " ";
+
+		for(int i = 0; i < SIZE_ARRAY; i ++){
+			
+			if(projects[i] != null){
+				if(projects[i].getFinalDate().after(dateToCompare)){
+					int pos = getFirstValidPositionForNames();
+					if(pos != -1){
+						namesAfterDate[pos] = projects[i].getName(); 
+					}
+				}
+		}	
+	}
+		for(int o = 0; o < 10; o++){
+
+			if(namesAfterDate[o] != null){
+				msg += namesAfterDate[o];
+				if(o < 10 - 1){
+					msg += ", ";
+				}
+			}
+		}
 
 		return msg;
-
 	}
 	
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsBeforeDate() {
+	public String searchProjectsBeforeDate(Calendar dateToCompare) {
 
-		String msg = "";
+		String msg = " ";
+
+		for(int i = 0; i < SIZE_ARRAY; i ++){
+			
+			if(projects[i] != null){
+				if(projects[i].getInitialDate().before(dateToCompare)){
+					int pos = getFirstValidPositionForNamesTwo();
+					if(pos != -1){
+						namesBeforeDate[pos] = projects[i].getName(); 
+					}
+				}
+		}	
+	}
+		for(int o = 0; o < 10; o++){
+
+			if(namesBeforeDate[o] != null){
+				msg += namesBeforeDate[o];
+				if(o < 10 - 1){
+					msg += ", ";
+				}
+			}
+		}
 
 		return msg;
+	
 
+	}
+
+	public int getFirstValidPosition(){
+		int pos = -1; 
+		boolean isFound = false; 
+		for(int i = 0; i < SIZE_ARRAY && !isFound; i++){
+			if(projects[i] == null){
+				pos = i; 
+				isFound = true;
+			}
+		}
+		return pos; 
+	}
+
+		public int getFirstValidPositionForNames(){
+		int pos = -1; 
+		boolean isFound = false; 
+		for(int i = 0; i < 10 && !isFound; i++){
+			if(namesAfterDate[i] == null){
+				pos = i; 
+				isFound = true;
+			}
+		}
+		return pos; 
+	}
+
+		public int getFirstValidPositionForNamesTwo(){
+		int pos = -1; 
+		boolean isFound = false; 
+		for(int i = 0; i < 10 && !isFound; i++){
+			if(namesBeforeDate[i] == null){
+				pos = i; 
+				isFound = true;
+			}
+		}
+		return pos; 
 	}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
 
 import java.util.Scanner;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import model.Controller;
 
 public class Main{
@@ -14,30 +16,151 @@ public class Main{
 		controller = new Controller();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Main exe = new Main();
-		exe.menu();
+		int option = 0;
+		do{	
 
+			exe.menu();
+			option = exe.validateIntegerInput();
+			exe.executeOption(option);
+
+		}while(option != 4);
 	}
 
 	// Incomplete
 	public void menu() {
 
+		System.out.println("---------------------------------------------------");
+		System.out.println("Welcome, please, choose an option.");
+		System.out.println(" ");
+		System.out.println("1. Regist project.");
+		System.out.println("2. Consult projects which ends after the date.");
+		System.out.println("3. Consult projects which start before the date.");
+		System.out.println("4. Exit.");
+		System.out.println(" ");
+		System.out.println("---------------------------------------------------");
 	}
 
-	//Incomplete
-	public void RegisterProject() {
+	public void executeOption(int option) throws Exception{
 
+		switch(option){
+
+			case 1:
+
+				System.out.println("You choosed regist project.");
+				System.out.println(" ");
+				registerProject();
+				break;
+			
+			case 2:
+
+				System.out.println("Follow the steps: ");
+				System.out.println(" ");
+				searchProjectsAfterDate();
+				break;
+			
+			case 3:
+
+				System.out.println("Follow the steps: ");
+				System.out.println(" ");
+				searchProjectsBeforeDate();
+				break;
+			
+			case 4: 
+
+				System.out.println("Goodbye.");
+				break;
+		}
 	}
 
-	//Incomplete
-	public void searchProjectsAfterDate() {
+    public int validateIntegerInput(){
+        int option = 0; 
+        if(reader.hasNextInt()){
+            option = reader.nextInt(); 
+        }
+        else{
+            reader.nextLine();
+            option = -1; 
+            System.out.println("Enter integer value."); 
+        }
+        return option; 
+    }
 
+	public void registerProject() throws Exception{
+
+		String name = " ";
+		String clientName = " ";
+		double budget = 0;
+		
+		System.out.println("Type the name of the project: ");
+		name = reader.next();
+		System.out.println("Type the name of the client: ");
+		clientName = reader.next();
+		System.out.println("Type the initial date of the project: ");
+		String initialDateStr = reader.next();
+		System.out.println("Type the final date of the project: ");
+		String finalDateStr =  reader.next();
+		System.out.println("Finally, type the budget of the project: ");
+		budget = reader.nextDouble();
+
+
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Calendar initialDate = Calendar.getInstance();
+		initialDate.setTime(format.parse(initialDateStr));
+		Calendar finalDate = Calendar.getInstance();
+		initialDate.setTime(format.parse(finalDateStr));
+
+		boolean confirmMessage = controller.registerProject(name, clientName, initialDate, finalDate, budget);
+		if(confirmMessage == true){
+			System.out.println("The project has been registed succesfully.");
+		}
+		else{
+			System.out.println("The project hasnt been registed.");
+		}
 	}
-	
-	//Incomplete
-	public void searchProjectsBeforeDate() {
 
+	public void searchProjectsAfterDate() throws Exception{
+		
+		String dateToEvaluateStr = " ";
+
+		System.out.println("Type the date to evaluate");
+		dateToEvaluateStr = reader.next();
+
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+		Calendar dateToEvaluate = Calendar.getInstance();
+		dateToEvaluate.setTime(format.parse(dateToEvaluateStr));
+
+		String confirmMessage = controller.searchProjectsAfterDate(dateToEvaluate);
+		if(confirmMessage != " "){
+			System.out.println("The projects that ends after " + dateToEvaluateStr + " are: " + confirmMessage);
+		}
+		else{
+			System.out.println("Wasnt find any project that ends after " + dateToEvaluateStr);
+		}
+	}
+
+	public void searchProjectsBeforeDate() throws Exception {
+
+		String dateToEvaluateStr = " ";
+
+		System.out.println("Type the date to evaluate");
+		dateToEvaluateStr = reader.next();
+
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+		Calendar dateToEvaluate = Calendar.getInstance();
+		dateToEvaluate.setTime(format.parse(dateToEvaluateStr));
+
+		String confirmMessage = controller.searchProjectsBeforeDate(dateToEvaluate);
+		if(confirmMessage != " "){
+		System.out.println("The projects that start before " + dateToEvaluateStr + " are: " + confirmMessage);
+		}
+		else{
+			System.out.println("Wasnt find any project that start before " + dateToEvaluateStr);
+		}
 	}
 }
